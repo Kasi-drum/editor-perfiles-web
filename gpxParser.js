b4f17035a2fcde0ade2ab53048feedce598b30fb
Kasi-drum <miaredel@gmail.com>
2026-08-21 12:34:01 +0200

    feat: gpxParser prioriza wpts dentro de trk, proximidad solo para globales

--- gpxParser.js
+++ gpxParser.js
@@ -1,18 +1,7 @@
 var gpxParser = {
   parseTracks: function(xmlString) {
-    const parser = new DOMParser();
-    const xml = parser.parseFromString(xmlString, 'text/xml');
-
-    const wpts = xml.querySelectorAll('wpt');
-    var waypoints = [];
-    wpts.forEach(function(pt) {
-      var lat = parseFloat(pt.getAttribute('lat'));
-      var lon = parseFloat(pt.getAttribute('lon'));
-      var name = pt.querySelector('name') ? pt.querySelector('name').textContent : '';
-      var eleEl = pt.querySelector('ele');
-      var ele = eleEl ? parseFloat(eleEl.textContent) : null;
-      waypoints.push({lat: lat, lng: lon, name: name, ele: ele, hasEle: !!eleEl});
-    });
+    var parser = new DOMParser();
+    var xml = parser.parseFromString(xmlString, 'text/xml');
 
     var zones = [];
     var zoneEls = xml.getElementsByTagName('zns:zone');
@@ -27,6 +16,31 @@ var gpxParser = {
         endEle: z.getAttribute('endEle') !== null ? parseFloat(z.getAttribute('endEle')) : null
       });
     }
+
+    var allWpts = xml.querySelectorAll('wpt');
+    var globalWaypoints = [];
+    var wptInTrack = {};
+
+    allWpts.forEach(function(pt) {
+      var lat = parseFloat(pt.getAttribute('lat'));
+      var lon = parseFloat(pt.getAttribute('lon'));
+      var name = pt.querySelector('name') ? pt.querySelector('name').textContent : '';
+      var eleEl = pt.querySelector('ele');
+      var ele = eleEl ? parseFloat(eleEl.textContent) : null;
+      var wp = {lat: lat, lng: lon, name: name, ele: ele, hasEle: !!eleEl};
+      if (pt.parentNode && pt.parentNode.localName === 'trk') {
+        var trkName = pt.parentNode.querySelector('name');
+        var key = trkName && trkName.textContent ? trkName.textContent.trim() : '';
+        if (key) {
+          if (!wptInTrack[key]) wptInTrack[key] = [];
+          wptInTrack[key].push(wp);
+        } else {
+          globalWaypoints.push(wp);
+        }
+      } else {
+        globalWaypoints.push(wp);
+      }
+    });
 
     var trks = xml.querySelectorAll('trk');
     var tracks = [];
@@ -54,26 +68,33 @@ var gpxParser = {
         trackpoints: trackpoints,
         totalDistance: accumulatedDist,
         maxEle: elevations.length ? Math.max.apply(null, elevations) : 0,
-        minEle: elevations.length ? Math.min.apply(null, elevations) : 0
+        minEle: elevations.length ? Math.min.apply(null, elevations) : 0,
+        waypoints: wptInTrack[name] || []
       });
     });
 
-    if (tracks.length > 1) {
-      for (var wi = 0; wi < waypoints.length; wi++) {
-        var wp = waypoints[wi];
-        var bestIdx = 0, bestDist = Infinity;
-        for (var ti = 0; ti < tracks.length; ti++) {
-          var tps = tracks[ti].trackpoints;
-          for (var pi = 0; pi < tps.length; pi++) {
-            var d = haversine(wp.lat, wp.lng, tps[pi].lat, tps[pi].lng);
-            if (d < bestDist) { bestDist = d; bestIdx = ti; }
-          }
+    for (var wi = 0; wi < globalWaypoints.length; wi++) {
+      var wp = globalWaypoints[wi];
+      var bestIdx = 0, bestDist = Infinity;
+      for (var ti = 0; ti < tracks.length; ti++) {
+        var tps = tracks[ti].trackpoints;
+        for (var pi = 0; pi < tps.length; pi++) {
+          var d = haversine(wp.lat, wp.lng, tps[pi].lat, tps[pi].lng);
+          if (d < bestDist) { bestDist = d; bestIdx = ti; }
         }
-        wp.trackName = tracks[bestIdx].name;
+      }
+      wp.trackName = tracks[bestIdx].name;
+      tracks[bestIdx].waypoints.push(wp);
+    }
+
+    var allWaypoints = [];
+    for (var ti = 0; ti < tracks.length; ti++) {
+      for (var wi = 0; wi < tracks[ti].waypoints.length; wi++) {
+        allWaypoints.push(tracks[ti].waypoints[wi]);
       }
     }
 
-    return { tracks: tracks, waypoints: waypoints, zones: zones };
+    return { tracks: tracks, waypoints: allWaypoints, zones: zones };
   },
   attachWaypoints: function(waypoints, trackpoints) {
     if (!trackpoints.length) return;
@@ -91,15 +112,6 @@ var gpxParser = {
     var parsed = this.parseTracks(xmlString);
 
     if (parsed.tracks.length > 1) {
-      for (var ti = 0; ti < parsed.tracks.length; ti++) {
-        var twps = [];
-        for (var wi = 0; wi < parsed.waypoints.length; wi++) {
-          if (parsed.waypoints[wi].trackName === parsed.tracks[ti].name) {
-            twps.push(parsed.waypoints[wi]);
-          }
-        }
-        parsed.tracks[ti].waypoints = twps;
-      }
       return {
         tracks: parsed.tracks,
         waypoints: parsed.waypoints,
